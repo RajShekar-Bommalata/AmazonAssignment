508a33c4fbdf7ef5f2299e225f82c374c0fe633c
Raja <Raja@DESKTOP-8248BF9>
2020-02-17 00:20:33 -0800

    Code Changes for product comparison in checkout screen

--- AmazonApp/src/test/java/pages/CheckoutScreen.java
+++ AmazonApp/src/test/java/pages/CheckoutScreen.java
@@ -4,36 +4,37 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.Reporter;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import utility.TestUtil;
 
 public class CheckoutScreen extends TestUtil {
 
-
-	@FindBy(xpath = "//*[@resource-id=\"com.amazon.mShop.android.shopping:id/item_title\"]")
+	@FindBy(xpath = "//android.view.View[@resource-id='spc-orders']/android.view.View[8]/android.view.View[4]")
 	private WebElement actualItemName;
 
-	
 	public CheckoutScreen(AndroidDriver<AndroidElement> driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+
 	/**
 	 * Get selected item name from CheckOut page
 	 */
 
 	public String getItemNameOnCheckOut() {
-		scrollDown();
-		scrollDown();
+		for (int scrollCount = 0; scrollCount < 5; scrollCount++) {
+			scrollDown();
+		}
 		String productNameInCheckout = "";
-		waitForElement(driver,actualItemName);
+		waitForElement(driver, actualItemName);
 		try {
 			productNameInCheckout = actualItemName.getText();
-			Reporter.log("Got item name from check out page");
+			logMessageWithScreenShot("Product name captured in checkout..", "checkout");
 		} catch (Exception e) {
 			Assert.fail("Failed to get item name from check out page");
+			logMessageWithScreenShot("Failed to get item name from check out page..", "Err_getItemNameOnCheckOut");
 		}
 		return productNameInCheckout;
 	}
