0a8c519187dfc4173193e3492229e156a0f9bf88
Raja <Raja@DESKTOP-8248BF9>
2020-02-17 00:33:40 -0800

    Code formatting done

--- AmazonApp/src/test/java/pages/PLP.java
+++ AmazonApp/src/test/java/pages/PLP.java
@@ -2,13 +2,10 @@ package pages;
 
 import java.util.List;
 
-
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.Reporter;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -18,7 +15,7 @@ public class PLP extends TestUtil {
 
 	@FindBy(xpath = "//*[@resource-id=\"com.amazon.mShop.android.shopping:id/item_title\"]")
 	private List<WebElement> productList;
-	
+
 	@FindBy(xpath = "//*[@resource-id='com.amazon.mShop.android.shopping:id/rs_corrections_mixed_quartz']")
 	private WebElement searchResults;
 
@@ -28,7 +25,8 @@ public class PLP extends TestUtil {
 	}
 
 	/**
-	 * This method fetches the desired product name and returns productNameInSearchPage
+	 * This method fetches the desired product name and returns
+	 * productNameInSearchPage
 	 */
 	public String getItemName() throws InterruptedException {
 		waitForElement(driver, searchResults);
@@ -38,12 +36,12 @@ public class PLP extends TestUtil {
 			for (WebElement list : productList) {
 				logMessage(list.getText());
 			}
-			logMessageWithScreenShot("Search results displayed","5_ProductList");
+			logMessageWithScreenShot("Search results displayed", "5_ProductList");
 			productNameInSearchPage = productList.get(2).getText();
 			logMessage("Retrieved product name at second location");
 		} catch (Exception e) {
 			Assert.fail("Failed to get item name from search results page");
-			logMessageWithScreenShot("Failed to get item name from search results page","Err_getItemName");
+			logMessageWithScreenShot("Failed to get item name from search results page", "Err_getItemName");
 		}
 		return productNameInSearchPage;
 	}
@@ -54,13 +52,12 @@ public class PLP extends TestUtil {
 	public void selectAProduct() {
 		try {
 			waitForElement(driver, productList.get(2));
-			//productNameInSearchPage = productList.get(2).getText();
+			// productNameInSearchPage = productList.get(2).getText();
 			productList.get(2).click();
-			logMessage(productList.get(2).getText() +"  Product is selected on search results page");
-		}
-		catch (Exception e) {
+			logMessage(productList.get(2).getText() + "  Product is selected on search results page");
+		} catch (Exception e) {
 			Assert.fail("Failed to select product");
-			logMessageWithScreenShot("Failed to select product","Err_selectAProduct");
+			logMessageWithScreenShot("Failed to select product", "Err_selectAProduct");
 		}
 	}
 }
