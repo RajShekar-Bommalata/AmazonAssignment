c28d477b748a4d83245158697d3cf6682120ab6e
RajShekar-Bommalata <37531427+RajShekar-Bommalata@users.noreply.github.com>
2020-02-17 08:22:32 -0800

    Commented code removed

--- AmazonApp/src/test/java/tests/ShippingDetails.java
+++ AmazonApp/src/test/java/tests/ShippingDetails.java
@@ -10,37 +10,6 @@ public class ShippingDetails extends TestBase {
 	@Test
 	public void selectOrEnterShippingDetails() throws Exception {
 		ShippingDetailsScreen shipping = new ShippingDetailsScreen(driver);
-		
-		/**
-		 * This code adds new shipping details
-		 */
-		
-		/*// Click on Add New Address Button
-		shipping.addNewAddress();
-		// Enter Full Name
-		shipping.enterFullName(datafile.getFullName());
-		// Enter Phone Number
-		shipping.enterPhoneNumber(datafile.getPhoneNumber());
-		// Enter Postal Code
-		shipping.enterPostalCode(datafile.getPostalCode());
-		// Enter City
-		shipping.enterCity(datafile.getCity());
-		// Enter State
-		shipping.enterState(datafile.getState());
-		// Enter AddressLine1
-		shipping.enterAddressLine1(datafile.getAddressLine1());
-		// Enter AddressLine2
-		shipping.enterAddressLine2(datafile.getAddressLine2());
-		// Enter Landmark
-		shipping.enterLandmark(datafile.getLandmark());
-		// Enter AddressLine1
-		Thread.sleep(3000);
-		shipping.hideKeyboard();
-		// Select Address type
-		shipping.clickOnAddressType();
-		// Select Home Address
-		shipping.selectAddressType();*/
-		
 		/**
 		 * This code selects default shipping address
 		 */
